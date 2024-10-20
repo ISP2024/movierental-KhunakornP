534e7554f7f46ac540d036a767b73f0b5de25bfe
Khunakorn Pattayakorn <khunakornp@gmail.com>
2024-10-20 11:06:08 +0700

    add Attributes to Movie, change movie to be a frozen dataclass

--- movie.py
+++ movie.py
@@ -1,17 +1,23 @@
 from pricing import NewRelease, RegularPrice, ChildrenPrice
+from typing import Collection
+from dataclasses import dataclass
 
 
+@dataclass(frozen=True)
 class Movie:
     """
     A movie available for rent.
     """
     
-    def __init__(self, title):
-        # Initialize a new movie. 
-        self.title = title
+    title = str
+    year = int
+    genre = Collection[str]
+
+    def is_genre(self, genre: str):
+        return genre.lower() in [x.lower() for x in self.genre]
 
     def get_title(self):
         return self.title
     
     def __str__(self):
-        return self.title
+        return f"{self.title} ({self.year})"
