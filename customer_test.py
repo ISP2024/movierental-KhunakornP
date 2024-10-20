537f23a1e15896e935ab6114454595c5629f6995
Khunakorn Pattayakorn <khunakornp@gmail.com>
2024-10-20 10:54:22 +0700

    update tests due to refactoring

--- customer_test.py
+++ customer_test.py
@@ -3,6 +3,7 @@ import unittest
 from customer import Customer
 from rental import Rental
 from movie import Movie
+from pricing import ChildrenPrice, RegularPrice, NewRelease
 
 
 class CustomerTest(unittest.TestCase):
@@ -15,19 +16,19 @@ class CustomerTest(unittest.TestCase):
 		movies = list of some movies
 		"""
 		self.c = Customer("Movie Mogul")
-		self.new_movie = Movie("Mulan", Movie.NEW_RELEASE)
-		self.regular_movie = Movie("CitizenFour", Movie.REGULAR)
-		self.childrens_movie = Movie("Frozen", Movie.CHILDRENS)
+		self.new_movie = Movie("Mulan")
+		self.regular_movie = Movie("CitizenFour")
+		self.childrens_movie = Movie("Frozen")
 
 	def test_billing(self):
 		"""Test the total charge of customers"""
 		customer = Customer("Window wayne")
 		# customer has not rented anything yet
 		self.assertEqual(customer.get_total_charge(), 0)
-		customer.add_rental(Rental(self.new_movie, 4))
+		customer.add_rental(Rental(self.new_movie, 4, NewRelease()))
 		# new movie costs 3 * 4 = 12$
 		self.assertEqual(customer.get_total_charge(), 12)
-		customer.add_rental(Rental(self.childrens_movie, 5))
+		customer.add_rental(Rental(self.childrens_movie, 5, ChildrenPrice()))
 		# also rent frozen for 5 days
 		# total charge = 1.5 + 2*1.5 + 12 = 16.5$
 		self.assertEqual(customer.get_total_charge(), 16.5)
@@ -36,10 +37,10 @@ class CustomerTest(unittest.TestCase):
 		"""Test the total points earned from renting"""
 		customer = self.c
 		self.assertEqual(customer.get_total_points(), 0)
-		customer.add_rental(Rental(self.childrens_movie, 5))
+		customer.add_rental(Rental(self.childrens_movie, 5, ChildrenPrice()))
 		# not a new movie only 1 point earned
 		self.assertEqual(customer.get_total_points(), 1)
-		customer.add_rental(Rental(self.new_movie, 5))
+		customer.add_rental(Rental(self.new_movie, 5, NewRelease()))
 		# new movie gains 1 point per day rented
 		self.assertEqual(customer.get_total_points(), 6)
 
@@ -51,7 +52,7 @@ class CustomerTest(unittest.TestCase):
 		self.assertIsNotNone(matches)
 		self.assertEqual("0.00", matches[1])
 		# add a rental
-		self.c.add_rental(Rental(self.new_movie, 4)) # days
+		self.c.add_rental(Rental(self.new_movie, 4, NewRelease())) # days
 		stmt = self.c.statement()
 		matches = re.match(pattern, stmt.replace('\n',''), flags=re.DOTALL)
 		self.assertIsNotNone(matches)
--- rental_test.py
+++ rental_test.py
@@ -2,58 +2,58 @@ import unittest
 from customer import Customer
 from rental import Rental
 from movie import Movie
+from pricing import ChildrenPrice, RegularPrice, NewRelease
 
 
 class RentalTest(unittest.TestCase):
 
 	def setUp(self):
-		self.new_movie = Movie("Dune: Part Two", Movie.NEW_RELEASE)
-		self.regular_movie = Movie("Air", Movie.REGULAR)
-		self.childrens_movie = Movie("Frozen", Movie.CHILDRENS)
+		self.new_movie = Movie("Dune: Part Two")
+		self.regular_movie = Movie("Air")
+		self.childrens_movie = Movie("Frozen")
 
 	def test_movie_attributes(self):
 		"""trivial test to catch refactoring errors or change in API of Movie"""
-		m = Movie("Air", Movie.REGULAR)
+		m = Movie("Air")
 		self.assertEqual("Air", m.get_title())
-		self.assertEqual(Movie.REGULAR, m.get_price_code())
 
 	def test_rental_price(self):
 		"""Test the price for different kinds of movies"""
 		# test new movies
-		rental = Rental(self.new_movie, 1)
+		rental = Rental(self.new_movie, 1, NewRelease())
 		self.assertEqual(rental.get_price(), 3.0)
-		rental = Rental(self.new_movie, 5)
+		rental = Rental(self.new_movie, 5, NewRelease())
 		self.assertEqual(rental.get_price(), 15.0)
 		# test regular movies
-		rental = Rental(self.regular_movie, 2)
+		rental = Rental(self.regular_movie, 2, RegularPrice())
 		self.assertEqual(rental.get_price(), 2.0)
-		rental = Rental(self.regular_movie, 5)
+		rental = Rental(self.regular_movie, 5, RegularPrice())
 		self.assertEqual(rental.get_price(), 6.5)
 		# test children movies
-		rental = Rental(self.childrens_movie, 2)
+		rental = Rental(self.childrens_movie, 2, ChildrenPrice())
 		self.assertEqual(rental.get_price(), 1.5)
-		rental = Rental(self.childrens_movie, 5)
+		rental = Rental(self.childrens_movie, 5, ChildrenPrice())
 		self.assertEqual(rental.get_price(), 4.5)
 
 	def test_rental_points(self):
 		"""Test the rental point gain for different movie types"""
 		# test children movies
-		rental = Rental(self.childrens_movie, 1)
+		rental = Rental(self.childrens_movie, 1, ChildrenPrice())
 		self.assertEqual(rental.rental_points(), 1)
 		# die hard frozen fans still only get 1 point per rental
-		rental = Rental(self.childrens_movie, 99)
+		rental = Rental(self.childrens_movie, 99, ChildrenPrice())
 		self.assertEqual(rental.rental_points(), 1)
 		# test regular movies
-		rental = Rental(self.regular_movie, 1)
+		rental = Rental(self.regular_movie, 1, RegularPrice())
 		self.assertEqual(rental.rental_points(), 1)
 		# still only 1 point per rental
-		rental = Rental(self.regular_movie, 420)
+		rental = Rental(self.regular_movie, 420, RegularPrice())
 		self.assertEqual(rental.rental_points(), 1)
 		# test new movies
-		rental = Rental(self.new_movie, 1)
+		rental = Rental(self.new_movie, 1, NewRelease())
 		self.assertEqual(rental.rental_points(), 1)
 		# new movies get more points per day rented
-		rental = Rental(self.new_movie, 50)
+		rental = Rental(self.new_movie, 50, NewRelease())
 		self.assertEqual(rental.rental_points(), 50)
 
 	def test_movies(self):
@@ -61,7 +61,5 @@ class RentalTest(unittest.TestCase):
 		Movies should be different objects but have the same PriceStrategy
 		if their types are the same.
 		"""
-		newer_movie = Movie("The minecraft movie", Movie.NEW_RELEASE)
+		newer_movie = Movie("The minecraft movie")
 		self.assertNotEqual(newer_movie, self.new_movie)
-		self.assertEqual(newer_movie.price_code, self.new_movie.price_code)
-		self.assertNotEqual(self.regular_movie.price_code, self.new_movie.price_code)
